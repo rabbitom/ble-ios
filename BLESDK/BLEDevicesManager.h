2a68501b26d043764b13c792731300500017d810
erabbit <rabbitom@gmail.com>
2017-08-13 21:04:50 +0800

    Initial Commit

---
+++ BLESDK/BLEDevicesManager.h
@@ -0,0 +1,30 @@
+//
+//  BLEDevicesManager.h
+//  BLESensor
+//
+//  Created by 郝建林 on 16/8/16.
+//  Copyright © 2016年 CoolTools. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreBluetooth/CoreBluetooth.h>
+
+@interface BLEDevicesManager : NSObject <CBCentralManagerDelegate>
+
++ (instancetype)sharedInstance;
++ (CBCentralManager*)central;
+
+- (void)addDeviceClassByParams: (NSDictionary*)params;
+
+- (void)loadDevices: (NSString*)filePath;
+
+- (void)searchDevices;
+- (void)stopSearching;
+//- (NSArray*)devicesOfClass: (NSString*)className sortBy: (NSString*)key max: (int)count;
+
+- (id)findDevice: (NSUUID*)deviceId;
+
+@property BOOL filterSearchByMainServices;
+@property BOOL filterDeviceOfUnknownClass;
+
+@end
---
+++ BLESDK/BLESDK.h
@@ -0,0 +1,22 @@
+//
+//  test.h
+//  test
+//
+//  Created by 郝建林 on 2016/11/2.
+//  Copyright © 2016年 CoolTools. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for test.
+FOUNDATION_EXPORT double testVersionNumber;
+
+//! Project version string for test.
+FOUNDATION_EXPORT const unsigned char testVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BLESDK/PublicHeader.h>
+
+#import <BLESDK/CoolUtility.h>
+#import <BLESDK/BLEUtility.h>
+#import <BLESDK/BLEDevice.h>
+#import <BLESDK/BLEDevicesManager.h>
